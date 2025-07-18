eb843cb70f5016cb8f4ac860e01c8c7055374bc3
Genowere Fortunate <genowerefortunate@gmail.com>
2025-07-18 16:36:03 +0300

    Add Challenge 5: Age group classification using user input

--- codechallengeset2/challenge5-ageGroup.js
+++ codechallengeset2/challenge5-ageGroup.js
@@ -1,1 +1,22 @@
-const readline 
+const readline = require("readline");
+
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const currentYear = new Date().getFullYear();
+
+r1.question("Enter your year of birth: ", function (year) {
+  const age = currentYear - parseInt(year);
+
+  if (age < 18) {
+    console.log("You are a minor.");
+  } else if (age >= 18 && age <= 36) {
+    console.log("You are a youth.");
+  } else {
+    console.log("You are an elder.");
+  }
+
+  r1.close();
+});
